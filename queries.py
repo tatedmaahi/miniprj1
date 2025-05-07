dac8721fb6ef9339ad5125f5f2630a92d0b3fccd
tatedmaahi <maahitated22@gmail.com>
2025-05-07 11:21:00 +0530

    Added unittest for Task Manager

--- queries.py
+++
@@ -1,9 +0,0 @@
-# SQL Queries for Task Management
-
-SELECT_ALL_TASKS = "SELECT * FROM tasks"
-SELECT_PENDING_TASKS = "SELECT * FROM tasks WHERE completed = 0"
-INSERT_TASK = "INSERT INTO tasks (description, priority, due_date) VALUES (?, ?, ?)"
-UPDATE_TASK_COMPLETED = "UPDATE tasks SET completed = ? WHERE id = ?"
-DELETE_TASK = "DELETE FROM tasks WHERE id = ?"
-COUNT_PENDING_TASKS = "SELECT COUNT(*) FROM tasks WHERE completed = 0"
-COUNT_COMPLETED_TASKS = "SELECT COUNT(*) FROM tasks WHERE completed = 1"
